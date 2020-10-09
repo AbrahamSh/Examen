40006f3761a02e6042064bc7bfb6a7dfd57a94ed
Abraham Shapiro <a.shapirof@gmail.com>
2020-10-08 21:57:14 -0500

    1

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Constants from 'expo-constants';
+import Quienessomos from './components/Quienessomos.js';
+import Contactanos from './components/Contactanos.js';
+import Ubicanos from './components/Ubicanos.js';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Quienes Somos" component={Quienessomos} />
+        <Tab.Screen name="Contactanos" component={Contactanos} />
+         <Tab.Screen name="Ubicanos" component={Ubicanos} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/Quinessomos.js
@@ -0,0 +1,43 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+
+
+export default function Ubicanos() {
+  return (
+    <View style={styles.container}>
+     <MapView style={styles.estiloMapa}
+     initialRegion={{
+       latitude:19.48,
+       longitude:-99.17,
+       latitudeDelta:35,
+       longitudeDelta:35
+     }}>
+     <Marker
+     coordinate={{
+       latitude:19.4018032,
+       longitude:-99.2640348
+     }}
+     description='Anahuac México Norte'
+     title='UAMN'
+     image={require('./assets/favicon.png')}
+     />
+     </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  estiloMapa:
+  {
+      width: Dimensions.get('window').width,
+      height:Dimensions.get('window').height,
+  }
+});
